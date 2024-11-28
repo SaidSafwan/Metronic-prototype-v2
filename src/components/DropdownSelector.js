1e03200cb3b543c63025d3f5b8c26151ee00bab9
SaidSafwan <saidsafwan59@gmail.com>
2024-11-28 23:49:32 +0530

    Add DropdownSector comp and css file

---
+++ src/components/DropdownSelector.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+import { initDropdowns } from "../utils/dropdown"; // Import utility
+import "./dropdown.css"; // Import dropdown styles
+import "./index.css"; 
+
+const DropdownSelector = ({ label, options }) => {
+    useEffect(() => {
+        initDropdowns(); // Initialize dropdown interactions
+    }, []);
+
+    return (
+        <div className="select-menu">
+            <div className="select-btn">
+                <span className="sBtn-text">{label}</span>
+                <i className="bx bx-chevron-down"></i>
+            </div>
+            <ul className="options">
+                {options.map((option, index) => (
+                    <li key={index} className={`option ${option === label ? "selected" : ""}`}>
+                        <span className="option-text">{option}</span>
+                        <i className="bx bx-check" style={{ color: "#1B84FF" }}></i>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default DropdownSelector;
