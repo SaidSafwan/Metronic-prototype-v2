efaf97725d4d315b53119b92da2ef62639747fe4
SaidSafwan <saidsafwan59@gmail.com>
2024-11-28 23:52:52 +0530

    Update Utils files

---
+++ src/utils/interactiveFeatures.js
@@ -0,0 +1,21 @@
+export const setupFeatureInteractions = () => {
+    const featureLinks = document.querySelectorAll(".feature-links li");
+
+    featureLinks.forEach((link) => {
+        link.addEventListener("click", () => {
+            featureLinks.forEach((item) => item.classList.remove("active"));
+            link.classList.add("active");
+        });
+    });
+
+    const filterItems = document.querySelectorAll(
+        ".filter-section .search-bar, .filter-section .select-menu .select-btn"
+    );
+
+    filterItems.forEach((item) => {
+        item.addEventListener("click", () => {
+            filterItems.forEach((el) => el.classList.remove("clicked"));
+            item.classList.add("clicked");
+        });
+    });
+};
