5f8bb0f3087329829aba79b71def1dca4fef2869
SaidSafwan <saidsafwan59@gmail.com>
2024-12-04 00:14:00 +0530

    Update Interactive Filter section

--- src/utils/interactiveFilters.js
+++ src/utils/interactiveFilters.js
@@ -1,16 +1,15 @@
 export const setupFilterItemInteractions = () => {
-    // Select all filter items in the filter section
+    // Select elements in the FilterSection
     const filterItems = document.querySelectorAll(
-        ".filter-section .search-bar, .select-menu .select-btn"
+        ".filter-section .search-bar, .filter-section .filter-btn, .filter-section .select-menu .select-btn"
     );
 
-    // Add click event listeners to each item
     filterItems.forEach((item) => {
         item.addEventListener("click", () => {
-            // Remove the 'clicked' class from all items
+            // Remove 'clicked' class from all filter items
             filterItems.forEach((el) => el.classList.remove("clicked"));
 
-            // Add the 'clicked' class to the clicked item
+            // Add 'clicked' class to the clicked item
             item.classList.add("clicked");
         });
     });
