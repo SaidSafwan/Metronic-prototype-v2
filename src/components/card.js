a2bdd988724af2fcc38a69532141979c7024c903
SaidSafwan <saidsafwan59@gmail.com>
2024-12-08 01:00:50 +0530

    Update card index wise design

--- src/components/card.js
+++ src/components/card.js
@@ -1,30 +1,38 @@
-import React from 'react';
-
-const Card = ({ image, title, address, area, beds, rating, price, location }) => (
-  <div className="cards">
-    <div className="card-left">
-      <img className="aprt-images" src={image} alt="resident" />
-    </div>
-    <div className="card-right">
-      <div className="img-title">
-        <div className="add-tag">
-          <i className="fa-solid fa-magnifying-glass"></i>
-          <a href="#">{title}</a>
+function Card({ image, title, location, size, beds, energy, price, agency, extraClass, index }) {
+    return (
+        <div className={`cards ${extraClass}`}>
+            <div className="card-left">
+                <img className="aprt-images" src={image} alt="resident" />
+            </div>
+            <div className="card-right">
+                <div className="img-title">
+                    <div className="add-tag">
+                        <i className="fa-solid fa-magnifying-glass"></i>
+                        <a href="#">{title}</a>
+                    </div>
+                    {/* Conditionally render heart icon */}
+                    <img
+                        src={index === 1 ? "./assets/icons/icons-heart-filled.svg" : "./assets/icons/icons-heart.svg"}
+                        className="like-icon"
+                        alt="heart-icon"
+                    />
+                </div>
+                <p className="aprt-info">{location}</p>
+                <div className="cards-icons">
+                    <img src="./assets/icons/icons-stretch-tool.svg" alt="size icon" />
+                    <span>{size}</span>
+                    <img src="./assets/icons/icons-single-bed.svg" alt="bed icon" />
+                    <span>{beds}</span>
+                    <img src="./assets/icons/icons-the-flash-sign.svg" style={{ width: "24px" }} alt="energy icon" />
+                    <span>{energy}</span>
+                </div>
+                <h2 className="price-tag">{price}</h2>
+                <div className="location-tag">
+                    <a href="#">{agency}</a>
+                </div>
+            </div>
         </div>
-        <img src="./icons/icons-heart.svg" className="like-icon" alt="heart-icon" />
-      </div>
-      <p className="aprt-info">{address}</p>
-      <div className="cards-icons">
-        <img src="./icons/icons-stretch-tool.svg" alt="icon" /><span>{area}</span>
-        <img src="./icons/icons-single-bed.svg" alt="icon" /><span>{beds}</span>
-        <img src="./icons/icons-the-flash-sign.svg" style={{ width: '24px' }} alt="icon" /><span>{rating}</span>
-      </div>
-      <h2 className="price-tag">{price}</h2>
-      <div className="location-tag">
-        <a href="#">{location}</a>
-      </div>
-    </div>
-  </div>
-);
+    );
+}
 
 export default Card;
