f01a212cd8fbd86a7aab8d0c36d75fd078472408
SaidSafwan <saidsafwan59@gmail.com>
2024-12-08 01:01:43 +0530

    Add SearchResultContainer Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import HeroContainer from  './HeroContainer';
 import Navbar from './navbar';
+import SearchResultsContainer from './SearchResultsContainer';
+
 
 function App() {
     return (
         <>
             <Navbar />
             <HeroContainer />
+            <SearchResultsContainer />
         </>
     );
 }
